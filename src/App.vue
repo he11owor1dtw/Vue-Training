e849ec31312ef91edb72beb1d0b90888c5930933
HelloCodeman <955khm@gmail.com>
2024-03-03 21:48:23 +0800

    練習Vue 樣板語法：標籤內文與屬性

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script setup>
-console.log("我的第一個組件");
+let message = "<u>Hello Vue</u>";
+let name = "content";
+let checked = false;
 </script>
 
 <template>
   <nav>導覽列</nav>
-  <main>主內容</main>
+  <main v-html="message.toUpperCase()"></main> <!--增加程式語句 -->
+  <main>{{ message.toUpperCase() }}</main> <!--增加程式語句 -->
+  <div :class="name">操作標籤的屬性</div>
+  <div :class="checked?'dark':'light'">操作標籤的屬性</div> <!--增加程式語句 JS三元運算式 -->
 </template>
 
 <style scoped>
 main {
   background-color: rgb(190, 190, 190);
+}
+
+.content {
+  color: blue;
+}
+
+.dark{
+  color: rgb(190, 22, 22);
+}
+
+.light{
+  color: rgb(66, 197, 197);
 }
 </style>
