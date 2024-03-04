e1860f7c2fa959dceff8192debf75c1494a0359d
HelloCodeman <955khm@gmail.com>
2024-03-04 14:16:22 +0800

    Vue 樣板語法：流程控制，判斷式與迴圈

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <script setup>
-let message = "<u>Hello Vue</u>";
-let name = "content";
-let checked = false;
+let level = 0;
+let topic = ["HTML", "CSS", "JavaScript", "Vue.js"];  // 陣列
+let topics = { html: "HTML", css: "CSS", js: "JavaScript", vue: "Vue.js" };  // 物件
 </script>
 
 <template>
   <nav>導覽列</nav>
-  <main v-html="message.toUpperCase()"></main> <!--增加程式語句 -->
-  <main>{{ message.toUpperCase() }}</main> <!--增加程式語句 -->
-  <div :class="name">操作標籤的屬性</div>
-  <div :class="checked?'dark':'light'">操作標籤的屬性</div> <!--增加程式語句 JS三元運算式 -->
+  <main>
+    <div v-if="level === 3">大神</div>
+    <div v-else-if="level === 2">資深</div>
+    <div v-else-if="level === 1">中階</div>
+    <div v-else>
+      <div>入門</div>
+      <ul>
+        <li v-for="(name, index) in topic">
+          {{ index }}-{{ name }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <div>入門</div>
+      <ul>
+        <li v-for="(value, key) in topics">  <!-- value=值 , key=鍵 -->
+          {{ key }}-{{ value }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <style scoped>
 main {
-  background-color: rgb(190, 190, 190);
-}
-
-.content {
-  color: blue;
-}
-
-.dark{
-  color: rgb(190, 22, 22);
-}
-
-.light{
-  color: rgb(66, 197, 197);
+  background-color: rgb(113, 223, 155);
 }
 </style>
