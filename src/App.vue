f8a75c558e2535cdbc7f34b64a55aebb415da03c
HelloCodeman <955khm@gmail.com>
2024-03-12 11:30:29 +0800

    Vue 組件結構：生命週期

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <script setup>
-import NavComp from "./Nav.vue";
 import MainComp from "./Main.vue";
 import { ref } from "vue";
-let subtitle = ref("這是網站的副標題");
-let updateSubtitle = function () {
-  subtitle.value = "這是新的的副標題"
+let visible = ref(true);
+let hide = function () {
+  visible.value = false;
+}
+let show = function () {
+  visible.value = true;
 }
 </script>
 
 <template>
-  <NavComp 
-  title="這是網站標題" 
-  :subtitle="subtitle">
-</NavComp>
-  <MainComp 
-  backgroundColor="black" 
-  color="white" 
-  @update="updateSubtitle"
-  ></MainComp>
+  <MainComp v-if="visible"></MainComp>
+  <button @click="hide">隱藏</button>
+  <button @click="show">顯示</button>
 </template>
 
 <style scoped></style>
