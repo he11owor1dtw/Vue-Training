425c4d1181403dd1362d291634a2999ae4bcc81d
HelloCodeman <955khm@gmail.com>
2024-03-14 15:02:38 +0800

    Vue 實務案例：Fetch API 串接後端資料

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <script setup>
-import NavComp from "./Nav.vue";
-import MainComp from "./Main.vue";
-import { ref } from "vue";
-let subtitle = ref("這是網站的副標題");
-let updateSubtitle = function () {
-  subtitle.value = "這是新的的副標題"
-}
+// 資料串接網址 https://cwpeng.github.io/live-records-samples/data/products.json
+import { onMounted, ref } from "vue";
+import List from "./List.vue";
+// 建立響應式狀態 products 一開始是空值 null
+let products = ref(null);
+
+// 組件掛載完成後，開始呼叫 fetch 串接後端的資料
+onMounted(async function () {
+  let response = await fetch("https://cwpeng.github.io/live-records-samples/data/products.json")
+  let data = await response.json();
+  // console.log(data);
+  products.value = data;
+})
 </script>
 
 <template>
-  <NavComp 
-  title="這是網站標題" 
-  :subtitle="subtitle">
-</NavComp>
-  <MainComp 
-  backgroundColor="black" 
-  color="white" 
-  @update="updateSubtitle"
-  ></MainComp>
+  <div class="headline">產品資料列表</div>
+  <div v-if="products === null">資料載入中</div>
+  <List v-else :products="products"></List>
 </template>
 
-<style scoped></style>
+<style scoped>
+.headline {
+  font-size: 20px;
+  font-weight: bolder;
+}
+
+
+</style>
---
+++ src/List.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps(['products']);
+</script>
+
+<template>
+  <div class="list">
+    <div class="product" v-for="product in products">
+      <div class="name">{{ product.name }}</div>
+      <div class="price">價格：{{ product.price }}</div>
+      <div class="description">{{ product.description }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.list {
+  line-height: 1.5rem;
+}
+
+.product {
+  padding: 15px 0px;
+  border-bottom: 1px solid lightgray;
+}
+
+.name {
+  font-weight: bolder;
+}
+</style>
